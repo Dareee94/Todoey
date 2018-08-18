f43bd79c91484b24528e4abcf66cd085aa3633d3
Darko Veselinov <darko@darkos-macbook-air.local>
2018-08-19 01:53:22 +1000

    Convert to MVC design pattern.

---
+++ Todoey/Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Darko Veselinov on 19/08/18.
+//  Copyright © 2018 Darko Veselinov. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title: String = ""
+    var done: Bool = false
+    
+}
